6e09f8461daa38292f04535d0207ed2e1de7df31
Joginder Mikael <jogindermikael@gmail.com>
2025-10-29 19:21:53 +0300

    Structuring contact service and JsonUtil.

--- src/main/java/git/joginder/mikael/ContactService.java
+++ src/main/java/git/joginder/mikael/ContactService.java
@@ -1,5 +1,35 @@
 package git.joginder.mikael;
 
-//Exporting to JSON file
+import java.nio.file.Path;
+import java.util.List;
+
+//All the contact service is handled here.
 public class ContactService {
+
+    Contact createContact(){
+        return null;
+    }
+
+    Contact getContact(int id){
+        return null;
+    }
+
+    List<Contact> getAllContacts(){
+       return null;
+    }
+    Contact upateContact(){
+        return null;
+    }
+
+    boolean deleteContact(int id){
+        return false;
+    }
+
+    Path exportAllToJson(Path file){
+        return null;
+    }
+
+    List<Contact> importFromJson(Path file){
+        return null;
+    }
 }
---
+++ src/main/java/git/joginder/mikael/JsonUtil.java
@@ -0,0 +1,24 @@
+package git.joginder.mikael;
+
+import java.util.List;
+
+//JSON Logic
+public class JsonUtil {
+
+    public String toJson(){
+        return null;
+    }
+
+    public void writeJsonToFile(){
+
+    }
+
+    public Contact fromJson(){
+        return null;
+    }
+
+    public List<Object> fromJsonList(){
+        return null;
+    }
+
+}
