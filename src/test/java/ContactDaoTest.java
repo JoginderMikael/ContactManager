d4f8291ebfffc43e7f7b767a37dcc3a770080052
Joginder Mikael <jogindermikael@gmail.com>
2025-11-06 16:54:40 +0300

    Update Tests

--- src/test/java/ContactDaoTest.java
+++ src/test/java/ContactDaoTest.java
@@ -1,21 +1,70 @@
 import git.joginder.mikael.Contact;
 import git.joginder.mikael.ContactDao;
+import git.joginder.mikael.DbUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ContactDaoTest {
 
-@Test
-    public void test(){
+    private ContactDao contactDao;
+    @BeforeEach
+            void setup(){
+        contactDao = new ContactDao();
+    }
 
-    ContactDao contactDao = new ContactDao();
+    @Test
+    public void ContactUpdateTest(){
+        //arrange
     Contact contact = new Contact();
-    contact.setEmail("Joginderi@gmail.com");
-    contact.setPhone(98499382);
-    contact.setName("Joginder MK");
+    contact.setEmail("Joginderi45@gmail.com");
+    contact.setPhone(98499384);
+    contact.setName("Joginder MK1");
 
-    contactDao.insertContact(contact);
+    //execute
+    boolean result = contactDao.insertContact(contact);
 
-    assert true;
+    //assert
+        assertTrue(result, "insertContact() should return true when insert succeeds");
+
+        //verify the record is in the DB
+        try(Connection conn = DbUtil.getConnection()) {
+            assertNotNull(conn);
+            try(PreparedStatement ps = conn.prepareStatement("SELECT * FROM contacts WHERE email = ?")){
+                ps.setString(1, "Joginderi45@gmail.com");
+                ResultSet rs = ps.executeQuery();
+
+                assertTrue(rs.next(), "Inserted contact exist in database");
+                assertEquals("Joginder MK1", rs.getString("name"));
+                assertEquals(98499384, rs.getInt("phone"));
+                assertEquals("Joginderi45@gmail.com", rs.getString("email"));
+            }
+        } catch(SQLException e){
+            fail("Database query failed: " + e.getMessage());
+        }
+
+
 }
+    @AfterEach
+    void tearDown(){
+        //cleaning up the inserted data
+        try(Connection conn = DbUtil.getConnection()) {
+            assertNotNull(conn);
+            try(PreparedStatement ps = conn.prepareStatement("DELETE FROM contacts WHERE email = ?")){
+                ps.setString(1, "Joginderi45@gmail.com");
+                ps.executeQuery();
+
+            }
+        } catch (SQLException e){
+            IO.println("Clean up failed: " + e.getMessage());
+        }
+    }
 
 }
